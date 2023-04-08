049dd7fa65ca584196a61b81575828e52c75383a
Brijen Makwana <brijenmakwana@192.168.1.7>
2023-04-08 09:47:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+    </div>
+  );
+}
+
+export default App;
